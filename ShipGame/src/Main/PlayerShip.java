ff72fee4af24b5cbe2145d38726b3348fd30b619
Ronny <Ronny@192.168.1.128>
2018-10-17 01:54:51 +0200

    Deleted extra stuff

--- ShipGame/src/Main/PlayerShip.java
+++ ShipGame/src/Main/PlayerShip.java
@@ -9,26 +9,24 @@ import javax.swing.JPanel;
 
 public class PlayerShip extends JPanel implements KeyListener, Runnable{
 //TODO Improve the drawing of the ship
+	
 	private static final long serialVersionUID = 1L;
+	
 	Point2D shipCenterPoint = new Point2D.Double(100,100);
 	Point2D.Double firstPoint = new Point2D.Double(shipCenterPoint.getX(),shipCenterPoint.getY()-20);
 	Point2D.Double secondPoint = new Point2D.Double(shipCenterPoint.getX()-15,shipCenterPoint.getY()+20);
-	Point2D.Double thirdPoint = new Point2D.Double(shipCenterPoint.getX()-15,shipCenterPoint.getY()+20);
-	Point2D.Double fourthPoint = new Point2D.Double(shipCenterPoint.getX(),shipCenterPoint.getY()+10);
-	Point2D.Double fithPoint = new Point2D.Double(shipCenterPoint.getX(),shipCenterPoint.getY()+10);
-	Point2D.Double sixthPoint = new Point2D.Double(shipCenterPoint.getX()+15,shipCenterPoint.getY()+20);
-	Point2D.Double seventhPoint = new Point2D.Double(shipCenterPoint.getX(),shipCenterPoint.getY()-20);
-	Point2D.Double eighthPoint = new Point2D.Double(shipCenterPoint.getX()+15,shipCenterPoint.getY()+20);
+	Point2D.Double thirdPoint = new Point2D.Double(shipCenterPoint.getX(),shipCenterPoint.getY()+10);
+	Point2D.Double fourthPoint = new Point2D.Double(shipCenterPoint.getX()+15,shipCenterPoint.getY()+20);
+	
 	int keyCode;
+	
 	Thread thread = new Thread(this);
+	
 	double x = shipCenterPoint.getX(), y = shipCenterPoint.getY(), velx=0, vely=0;
 	double x1 = shipCenterPoint.getX(), y1 = shipCenterPoint.getY();
 	double x2 = shipCenterPoint.getX(), y2 = shipCenterPoint.getY();
 	double x3 = shipCenterPoint.getX(), y3 = shipCenterPoint.getY();
-	double x4 = shipCenterPoint.getX(), y4 = shipCenterPoint.getY();
-	double x5 = shipCenterPoint.getX(), y5 = shipCenterPoint.getY();
-	double x6 = shipCenterPoint.getX(), y6 = shipCenterPoint.getY();
-	double x7 = shipCenterPoint.getX(), y7 = shipCenterPoint.getY();
+
 	double shipSize = 40;
 	
 	public PlayerShip() {
@@ -46,9 +44,9 @@ public class PlayerShip extends JPanel implements KeyListener, Runnable{
 		g2.setColor(Color.BLACK);
 		//g2.setStroke(new BasicStroke(3.7f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
 		g2.draw(new Line2D.Double(x, y, x1, y1));
+		g2.draw(new Line2D.Double(x1, y1, x2, y2));
 		g2.draw(new Line2D.Double(x2, y2, x3, y3));
-		g2.draw(new Line2D.Double(x4, y4, x5, y5));
-		g2.draw(new Line2D.Double(x6, y6, x7, y7));
+		g2.draw(new Line2D.Double(x, y, x3, y3));
 		
 		g2.setColor(Color.RED);
 		g2.drawOval((int)shipCenterPoint.getX(), (int)shipCenterPoint.getY(), 1, 1);
@@ -115,22 +113,13 @@ public class PlayerShip extends JPanel implements KeyListener, Runnable{
 			y = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, firstPoint).y;
 			x1 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), velx, secondPoint).x;
 			y1 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, secondPoint).y;
-			
+	
 			x2 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), velx, thirdPoint).x;
-			y2 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, thirdPoint).y;	
+			y2 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, thirdPoint).y;
+					
 			x3 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), velx, fourthPoint).x;
 			y3 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, fourthPoint).y;
-			
-			x4 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), velx, fithPoint).x;
-			y4 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, fithPoint).y;	
-			x5 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), velx, sixthPoint).x;
-			y5 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, sixthPoint).y;
-			
-			x6 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), velx, seventhPoint).x;
-			y6 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, seventhPoint).y;	
-			x7 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), velx, eighthPoint).x;
-			y7 = rotatePoint(shipCenterPoint.getX(),shipCenterPoint.getY(), vely, eighthPoint).y;
-
+						
 			repaint();
 			Thread.sleep(50);
 			
