cfa9e3c5b16e4b98344882c8e1d7d4c3040a6d4a
Ronny <Ronny@192.168.1.128>
2018-10-21 03:33:28 +0200

    Fixed some code

--- ShipGame/src/game/prototype/Handler.java
+++ ShipGame/src/game/prototype/Handler.java
@@ -35,46 +35,43 @@ public class Handler extends KeyAdapter{
 	public void updateInput() {
 		//Pressed
 		if(upKeyPressed == true) {
-			up();
+			movement('u');
 			if(upKeyPressed == true && leftKeyPressed == true) {
-				up();
-				left();
-				if(leftKeyReleased == true) {
-					rotationStop();
-				}		
+				movement('u');
+				movement('l');
 			}
 			else if(upKeyPressed == true && rightKeyPressed == true) {
-				up();
-				right();
+				movement('u');
+				movement('r');
 			}
 			else if(leftKeyReleased == true) {
-				rotationStop();
+				desacelerate('l');
 			}
 			else if(rightKeyReleased == true) {
-				rotationStop();
+				desacelerate('r');
 			}
 		}			
 		if (downKeyPressed == true) {
-			down();
+			movement('d');
 		}
 		if (leftKeyPressed == true) {
-			left();
+			movement('l');
 		}
 		if (rightKeyPressed == true) {
-			right();
+			movement('r');
 		}
 		//Released
 		if (upKeyReleased == true) {
-			desacelerate(-1);		
+			desacelerate('u');		
 		}
 		if (downKeyReleased == true) {
-			desacelerate(1);		
+			desacelerate('d');		
 		}
 		if (leftKeyReleased == true && leftKeyPressed == false && rightKeyPressed == false) {	
-			rotationStop();
+			desacelerate('l');
 		}
 		if (rightKeyReleased == true && rightKeyPressed == false && leftKeyPressed == false) {	
-			rotationStop();
+			desacelerate('r');
 		}
 		
 	}
@@ -91,7 +88,6 @@ public class Handler extends KeyAdapter{
 	}
 	
 	public void keyPressed(KeyEvent e) {
-
 		for(int i = 0; i < object.size(); i++) {
 			tempObject = object.get(i);
 			if(tempObject.getId() == ObjectId.PlayerShip) {	
@@ -99,25 +95,24 @@ public class Handler extends KeyAdapter{
 					upKeyPressed = true;
 					upKeyReleased = false;
 				}
-				else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 					downKeyPressed = true;
 					downKeyReleased = false;
 				}
-				else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 					leftKeyPressed = true;	
 					leftKeyReleased = false;	
 				}
-				else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 					rightKeyPressed = true;
 					rightKeyReleased = false;
 					
 				}
-				else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
 					System.exit(1);
 				}
 			}
-		}
-			
+		}			
 	}
 
 	public void keyReleased(KeyEvent e) {	
@@ -128,76 +123,95 @@ public class Handler extends KeyAdapter{
 					upKeyPressed = false;
 					upKeyReleased = true;
 				}
-				else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 					downKeyPressed = false;
 					downKeyReleased = true;
 				}
-				else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 					leftKeyPressed = false;
 					leftKeyReleased = true;	
 				}
-				else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 					rightKeyPressed = false;
 					rightKeyReleased = true;		
 				}
 			}
 		}
 	}
-	
-	private void up() {
-		vely += deltaSpeed;
-		tempObject.setVelY(vely);
-		if (vely >= topSpeed) {
-			vely = (float) topSpeed;
+	private void movement(char move) {
+		switch(move) 
+		{
+		case 'u':
+			vely += deltaSpeed;
 			tempObject.setVelY(vely);
-		}
-	}
-	private void down() {
-		vely += -deltaSpeed*2;
-		tempObject.setVelY(vely);
-		if (vely <= -topSpeed/10) {
-			vely = (float) -topSpeed/10;
+			if (vely >= topSpeed) {
+				vely = (float) topSpeed;
+				tempObject.setVelY(vely);
+			}
+			break;
+		case 'd':
+			vely += -deltaSpeed*2;
 			tempObject.setVelY(vely);
+			if (vely <= -topSpeed/10) {
+				vely = (float) -topSpeed/10;
+				tempObject.setVelY(vely);
+			}
+			break;
+		case 'l':
+			velx -= deltaSpeed;
+			tempObject.setVelX(velx);
+			if (velx <= -topSpeed/100) {
+				velx = (float) (-topSpeed/100);
+				tempObject.setVelX(velx);
+			}	
+			break;
+		case 'r':
+			velx += deltaSpeed;
+			tempObject.setVelX(velx);
+			if (velx >= topSpeed/100) {
+				velx = (float) (topSpeed/100);
+				tempObject.setVelX(velx);
+			}
+			break;
 		}
 	}
-	private void left() {
-		velx -= deltaSpeed;
-		tempObject.setVelX(velx);
-		if (velx <= -topSpeed/100) {
-			velx = (float) (-topSpeed/100);
-			tempObject.setVelX(velx);
-		}	
-	}
-	private void right() {
-		velx += deltaSpeed;
-		tempObject.setVelX(velx);
-		if (velx >= topSpeed/100) {
-			velx = (float) (topSpeed/100);
-			tempObject.setVelX(velx);
-		}
-	}
-	private void rotationStop() {
-		velx = 0;
-		tempObject.setVelX(velx);
-	}
-	//TODO implement rotation desaceleration
-	private void desacelerate(int direction) {
-		if(direction < 0) {
-			vely += deltaSpeed/5 * direction;
+	private void desacelerate(char direction) {	
+		switch(direction)
+		{
+		case 'u':	
+			vely -= deltaSpeed/5;
 			tempObject.setVelY(vely);
 			if (vely < 0) {
 				vely = 0;
 				tempObject.setVelY(vely);
 				upKeyReleased = false; 
 			}
-		}
-		if(direction > 0) {
-			vely += deltaSpeed/5 * direction;
+			break;
+		case 'd':
+			vely += deltaSpeed/5;
 			tempObject.setVelY(vely);
 			if (vely > 0) {
 				vely = 0;
 				tempObject.setVelY(vely);
 				downKeyReleased = false;
+			}
+			break;
+		case 'l':
+			velx += deltaSpeed/50;
+			tempObject.setVelX(velx);
+			if(velx > 0) {
+				velx = 0;
+				tempObject.setVelX(velx);
+				leftKeyReleased = false;
+			}
+			break;
+		case 'r':
+			velx -= deltaSpeed/50;
+			tempObject.setVelX(velx);
+			if(velx < 0) {
+				velx = 0;
+				tempObject.setVelX(velx);
+				rightKeyReleased = false;
 			}
 		}
 	}
