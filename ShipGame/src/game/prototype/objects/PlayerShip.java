da0262dbbbd20abb5aa796fe47c39e0512d400d7
Ronny <Ronny@192.168.1.128>
2018-10-24 22:53:23 +0200

    Improved the bounds around the screen

--- ShipGame/src/game/prototype/objects/PlayerShip.java
+++ ShipGame/src/game/prototype/objects/PlayerShip.java
@@ -2,10 +2,15 @@ package game.prototype.objects;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.util.LinkedList;
 
 import game.prototype.Game;
@@ -21,52 +26,44 @@ public class PlayerShip extends GameObject {
 	}
 	
 	private Handler handler;
-	
-	private Point2D.Double center = new Point2D.Double(x,y);
-	//Ship points respect the ship center point.
-	private Point2D.Double firstPoint = new Point2D.Double(x,            y-(sizeY/2));
-	private Point2D.Double secondPoint = new Point2D.Double(x-(sizeX/2), y+(sizeY/2));
-	private Point2D.Double thirdPoint = new Point2D.Double(x,            y+(sizeY/3));
-	private Point2D.Double fourthPoint = new Point2D.Double(x+(sizeX/2), y+(sizeY/2));
-	//Points for bound circles
-	private Point2D.Double boundTopCenter = new Point2D.Double(x,                y-(sizeY/3));
-	private Point2D.Double boundLeftCenter = new Point2D.Double(x-(sizeX*0.37),  y+(sizeY/5*2));
-	private Point2D.Double boundRightCenter = new Point2D.Double(x+(sizeX*0.37), y+(sizeY/5*2));
 	//Bound circle dimension
-	private double ellipse = sizeX/4;
+	private double ellipse = sizeX;
+	//Ship points respect the ship center point.
+	private Point2D.Double center = new Point2D.Double(x, y);
+	private Point2D.Double firstPoint = new Point2D.Double(center.x,            center.y-(sizeY/2));
+	private Point2D.Double secondPoint = new Point2D.Double(center.x-(sizeX/2), center.y+(sizeY/2));
+	private Point2D.Double thirdPoint = new Point2D.Double(center.x,            center.y+(sizeY/3));
+	private Point2D.Double fourthPoint = new Point2D.Double(center.x+(sizeX/2), center.y+(sizeY/2));
+	//Point for bound circle
+	private Point2D.Double boundCircleCenter = center;
+	private Point2D.Double velComp = new Point2D.Double(), oldPos = new Point2D.Double(), newPos = new Point2D.Double();
+
 	
 	public void update(LinkedList<GameObject> object) {
 		//Move forward and backwards
+	
 		if (CollisionVsScreen(object) == false) {
 			shipMovement();
 		}else{
+			oldPos = firstPoint;
 			shipMovement();
+			newPos = firstPoint;
+			velComp.x = newPos.x-oldPos.x;
+			velComp.y = newPos.y-oldPos.y;
 		}
 	}
 
 	public void render(Graphics2D g2) {
-		
-		double xpoints[] = {firstPoint.x,secondPoint.x,thirdPoint.x,fourthPoint.x};
-		double ypoints[] = {firstPoint.y,secondPoint.y,thirdPoint.y,fourthPoint.y};
-		Path2D polygon = new Path2D.Double();
-		
-		polygon.moveTo(xpoints[0], ypoints[0]);
-		for(int i = 1; i < xpoints.length; ++i) {
-			polygon.lineTo(xpoints[i], ypoints[i]);
-		}
-		polygon.closePath();
 		//Enable AA
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		//g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);	
 		g2.setColor(Color.BLACK);
 		//g2.setStroke(new BasicStroke(3.7f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
-		g2.draw(polygon);
-		//g2.fill(polygon);
+		g2.draw(ship());
+		//g2.fill(ship());
 		g2.drawOval((int)center.x-3, (int)center.y-3, 6, 6);	
 		g2.setColor(Color.RED);
 		g2.draw(getBounds());
-		g2.draw(getBoundsLeft());
-		g2.draw(getBoundsRight());
 	}
 	
 	private boolean CollisionVsScreen(LinkedList<GameObject> object) {
@@ -75,83 +72,57 @@ public class PlayerShip extends GameObject {
 			
 			if(tempObject.getId() == ObjectId.PlayerShip) {
 				double r = ellipse/2;
-				//collide vs. x-bounds
-				double top_dx = 0 - (boundTopCenter.x - r);
-				double left_dx = 0 - (boundLeftCenter.x - r);
-				double right_dx = 0 - (boundRightCenter.x - r);
-				if (0<top_dx) {
-					//TODO Physics HERE
-					velY -=5;
-					return true;
-				}else{
-					top_dx = (boundTopCenter.x + r)-Game.WIDTH;
-					if(0<top_dx) {
-						//TODO Physics HERE
-						velY -=5;
+				double dx = -(boundCircleCenter.x - r);
+				double dy = -(boundCircleCenter.y - r);
+				//collide vs. x-bounds	
+				if (0<dx) {
+					CollisionVsScreenReaction(dx, 0, 1, 0, boundCircleCenter);
+					center = boundCircleCenter;
+					//Check if reach corner
+					if(0<dy) {
+						CollisionVsScreenReaction(0, dy, 0, 1, boundCircleCenter);
+						center = boundCircleCenter;
 						return true;
+					}else{
+						dy = (boundCircleCenter.y + r)-Game.HEIGHT;
+						if(0<dy) {
+							CollisionVsScreenReaction(0, -dy, 0, -1, boundCircleCenter);
+							center = boundCircleCenter;
+							return true;
+						}	
 					}
-				}
-				if(0<left_dx) {
-					//TODO Physics HERE
-					velX -= 0.15;
 					return true;
 				}else{
-					left_dx = (boundLeftCenter.x + r)-Game.WIDTH;
-					if(0<left_dx) {
-						//TODO Physics HERE
-						velX -= 0.15;
-						return true;
-					}
-				}
-				if(0<right_dx) {
-					//TODO Physics HERE
-					velX += 0.15;
-					return true;
-				}else{
-					right_dx = (boundRightCenter.x + r)-Game.WIDTH;
-					if(0<right_dx) {
-						//TODO Physics HERE
-						velX += 0.15;
+					dx = (boundCircleCenter.x + r)-Game.WIDTH;
+					if(0<dx) {
+						CollisionVsScreenReaction(-dx, 0 , -1, 0, boundCircleCenter);
+						center = boundCircleCenter;
+						//Check if reach corner
+						if(0<dy) {
+							CollisionVsScreenReaction(0, dy, 0, 1, boundCircleCenter);
+							center = boundCircleCenter;
+							return true;
+						}else{
+							dy = (boundCircleCenter.y + r)-Game.HEIGHT;
+							if(0<dy) {
+								CollisionVsScreenReaction(0, -dy, 0, -1, boundCircleCenter);
+								center = boundCircleCenter;
+								return true;
+							}	
+						}
 						return true;
 					}
 				}
 				//collide vs. y-bounds
-				double top_dy = 0 - (boundTopCenter.y - r);
-				double left_dy = 0 - (boundLeftCenter.y - r);
-				double right_dy = 0 - (boundRightCenter.y - r);
-				if (0<top_dy) {
-					//TODO Physics HERE
-					velY -=5;
+				if (0<dy) {
+					CollisionVsScreenReaction(0, dy, 0, 1, boundCircleCenter);
+					center = boundCircleCenter;
 					return true;
 				}else{
-					top_dy = (boundTopCenter.y + r)-Game.HEIGHT;
-					if(0<top_dy) {
-						//TODO Physics HERE
-						velY -=5;
-						return true;
-					}
-				}
-				if (0<left_dy) {
-					//TODO Physics HERE
-					velX -= 0.15;
-					return true;
-				}else{
-					left_dy = (boundLeftCenter.y + r)-Game.HEIGHT;
-					if(0<left_dy) {
-						//TODO Physics HERE
-						velX -= 0.15;
-						return true;
-					}
-				}
-				if (0<right_dy) {
-					//TODO Physics HERE
-					velX += 0.15;
-					return true;
-				}else{
-					right_dy = (boundRightCenter.y + r)-Game.HEIGHT;
-					if(0<right_dy) {
-						//TODO Physics HERE
-						velX += 0.15;
+					dy = (boundCircleCenter.y + r)-Game.HEIGHT;
+					if(0<dy) {
+						CollisionVsScreenReaction(0, -dy, 0, -1, boundCircleCenter);
+						center = boundCircleCenter;
 						return true;
 					}
 				}
@@ -161,43 +132,93 @@ public class PlayerShip extends GameObject {
 	}
 	
 	public Ellipse2D getBounds() {
-		return new Ellipse2D.Double(boundTopCenter.x-ellipse/2, boundTopCenter.y-ellipse/2, ellipse, ellipse);
+		return new Ellipse2D.Double(boundCircleCenter.x-ellipse/2, boundCircleCenter.y-ellipse/2, ellipse, ellipse);
 	}
-	public Ellipse2D getBoundsLeft() {
-		return new Ellipse2D.Double(boundLeftCenter.x-ellipse/2, boundLeftCenter.y-ellipse/2, ellipse, ellipse);
-	}
-	public Ellipse2D getBoundsRight() {
-		return new Ellipse2D.Double(boundRightCenter.x-ellipse/2, boundRightCenter.y-ellipse/2, ellipse, ellipse);
-	}
+	
+	private void CollisionVsScreenReaction(double px, double py, double dx, double dy, Point2D.Double position) {
 
-	private void shipMovement() {
-		center = movePoint(velY, center, center);
+		double vx = velComp.x;
+		double vy = velComp.y;
+		double dp = (vx*dx + vy*dy);//project velocity onto collision normal
+		 
+		double nx = dp*dx;//nx,ny is normal velocity
+		double ny = dp*dy;
+			
+		double tx = vx-nx;//px,py is tangent velocity
+		double ty = vy-ny;
+
+		//we only want to apply collision response forces if the object is travelling into, and not out of, the collision
+		double b,bx,by,f,fx,fy;
+		 
+		if(dp < 0)
+			{
+				f = 0.05;
+				fx = tx*f;
+				fy = ty*f;		
+				
+				b = 1+0.3;//this bounce constant should be elsewhere, i.e inside the object/tile/etc..
+				
+				bx = nx*b;
+				by = ny*b;	
+			}else{
+				//moving out of collision, do not apply forces
+				bx = by = fx = fy = 0;
+			}
 		
-		firstPoint = movePoint(velY, center, firstPoint);
-		secondPoint = movePoint(velY, center, secondPoint);
-		thirdPoint = movePoint(velY, center, thirdPoint);
-		fourthPoint = movePoint(velY, center, fourthPoint);
-		
-		boundTopCenter = movePoint(velY,center, boundTopCenter);
-		boundLeftCenter = movePoint(velY,center, boundLeftCenter);
-		boundRightCenter = movePoint(velY,center, boundRightCenter);
-		//Rotate left and right
-		firstPoint = rotatePoint(velX, center, firstPoint);
-		secondPoint = rotatePoint(velX, center, secondPoint);
-		thirdPoint = rotatePoint(velX, center, thirdPoint);
-		fourthPoint = rotatePoint(velX, center, fourthPoint);
-		
-		boundTopCenter = rotatePoint(velX,center, boundTopCenter);
-		boundLeftCenter = rotatePoint(velX,center, boundLeftCenter);
-		boundRightCenter = rotatePoint(velX,center, boundRightCenter);		
+		position.x += px;
+		position.y += py;
+		 
+		firstPoint.x += px;
+		firstPoint.y += py;
+		secondPoint.x += px;
+		secondPoint.y += py;
+		thirdPoint.x += px;
+		thirdPoint.y += py;
+		fourthPoint.x += px;
+		fourthPoint.y += py;
 	}
-	private Point2D.Double movePoint(double speed, Point2D.Double center, Point2D.Double point) {
+	
+	private Path2D ship() {
+		double xpoints[] = {firstPoint.x,secondPoint.x,thirdPoint.x,fourthPoint.x};
+		double ypoints[] = {firstPoint.y,secondPoint.y,thirdPoint.y,fourthPoint.y};
+		Path2D polygon = new Path2D.Double();
+		
+		polygon.moveTo(xpoints[0], ypoints[0]);
+		for(int i = 1; i < xpoints.length; ++i) {
+			polygon.lineTo(xpoints[i], ypoints[i]);
+		}
+		polygon.closePath();
+		return polygon;
+	}
+	
+	private void shipMovement() {
+		//Move forward and backward
+		center = movement(velY, center, center);
+		firstPoint = movement(velY, center, firstPoint);
+		secondPoint = movement(velY, center, secondPoint);
+		thirdPoint = movement(velY, center, thirdPoint);
+		fourthPoint = movement(velY, center, fourthPoint);
+	
+		boundCircleCenter = movement(velY,center, boundCircleCenter);
+
+		//Rotate left and right
+		firstPoint = direction(velX, center, firstPoint);
+		secondPoint = direction(velX, center, secondPoint);
+		thirdPoint = direction(velX, center, thirdPoint);
+		fourthPoint = direction(velX, center, fourthPoint);
+
+		boundCircleCenter = direction(velX,center, boundCircleCenter);
+	}
+	
+	private Point2D.Double movement(double speed, Point2D.Double center, Point2D.Double point) {
+
 		double xnew = point.x, ynew = point.y;
 		xnew += speed * Math.sin(Math.atan2(firstPoint.x - center.x, firstPoint.y - center.y));
 		ynew += speed * Math.cos(Math.atan2(firstPoint.x - center.x, firstPoint.y - center.y));
 		return new Point2D.Double(xnew,ynew);	
 	}
-	private Point2D.Double rotatePoint(double angle, Point2D.Double center, Point2D.Double point) {
+	
+	private Point2D.Double direction(double angle, Point2D.Double center, Point2D.Double point) {
 		point.x -= center.x;
 		point.y -= center.y;
 		double xnew = point.x * Math.cos(angle) -point.y * Math.sin(angle);
