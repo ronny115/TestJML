6956039fe6eaf482ea32af05ca9d37f615f18032
Ronny <Ronny@192.168.1.128>
2018-10-31 20:12:38 +0100

    Max. and min. values functions

--- ShipGame/src/game/prototype/objects/Tile.java
+++ ShipGame/src/game/prototype/objects/Tile.java
@@ -23,13 +23,10 @@ public class Tile extends GameObject {
 	
 	private float[] xpoints, ypoints;
 	private Point2D.Float A = new Point2D.Float(400,400);
-	private Point2D.Float B = new Point2D.Float(606,606);
+	private Point2D.Float B = new Point2D.Float(600,600);
 	private Point2D.Float C = new Point2D.Float();
-	private Point2D.Float D = new Point2D.Float();
 	private Point2D.Float E = new Point2D.Float();
 	private Point2D.Float X = new Point2D.Float((float)(-300* Math.cos(angle(A.x,A.y,B.x,B.y))+A.x),(float)(300* Math.sin(angle(A.x,A.y,B.x,B.y)))+A.y);
-	private Point2D.Float vecAB = new Point2D.Float();
-	private Point2D.Float vecAC = new Point2D.Float();
 	private Point2D.Float vecXA = new Point2D.Float();
 	private Point2D.Float vecXC = new Point2D.Float();
 	private boolean collide = false;
@@ -38,21 +35,16 @@ public class Tile extends GameObject {
 		
 	}
 	
-	
-
 	public void render(Graphics2D g2) {
 		//Enable AA
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		//g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);	
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);	
 		g2.setColor(Color.BLACK);
-		//g2.setStroke(new BasicStroke(3.7f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
 		g2.draw(tile());
-		//AB
+
 		g2.setColor(Color.red);
 		g2.drawLine((int)A.x,(int)A.y, (int)B.x, (int)B.y);
 		g2.setColor(Color.blue);
-		//AC
-		//angle(A.x,A.y,B.x,B.y);
+
 		g2.drawLine((int)A.x,(int)A.y,(int)(X.x), (int)(X.y));
 		g2.setColor(Color.orange);
 
@@ -66,115 +58,88 @@ public class Tile extends GameObject {
 		for(int i = 0; i < handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);			
 			if(tempObject.getId() == ObjectId.PlayerShip) {
+				
 				xpoints = tempObject.getxPoints();
 				ypoints = tempObject.getyPoints();
-				float projectionOnAB_points_x[] = new float[xpoints.length];
-				float projectionOnAB_points_y[] = new float[xpoints.length];
-				float projectionOnAX_points_x[] = new float[xpoints.length];
-				float projectionOnAX_points_y[] = new float[xpoints.length];
-				float ship_points_x[] = new float[xpoints.length];
-				float ship_points_y[] = new float[xpoints.length];
+				//px and py are the arrays of ship points projected on the perpendicular line AX
+				float px[] = new float[xpoints.length], py[] = new float[xpoints.length];
+				//sx and sy are the arrays of ship points
+				float sx[] = new float[xpoints.length], sy[] = new float[xpoints.length]; 
+				
 				for(int j = 0; j<xpoints.length;j++) {
-					C.x = (float) xpoints[j];
-					C.y = (float) ypoints[j];
-					//Projection on vecAB
-					vecAB = handler.vectorize(A, B);
-					vecAC = handler.vectorize(A, C);
-					D = projection(vecAB,vecAC);
-					D.x = D.x + A.x;
-					D.y = D.y + A.y;
-					//Projection on vecAX
+					C.x = xpoints[j];
+					C.y = ypoints[j];
+
 					vecXA = handler.vectorize(X, A);
 					vecXC = handler.vectorize(X, C);
 					E = projection(vecXA, vecXC);
 					E.x = E.x + X.x;
-					E.y = E.y + X.y;
+					E.y = E.y + X.y;				
 					
-					
-					
-					projectionOnAX_points_x[j] = E.x;
-					projectionOnAX_points_y[j] = E.y;
-					
-					projectionOnAB_points_x[j] = D.x;
-					projectionOnAB_points_y[j] = D.y;
-					
-					ship_points_x[j] = C.x;
-					ship_points_y[j] = C.y;
-					
-					//g2.drawLine((int)C.x,(int)C.y, (int)(E.x), (int)(E.y));
-					//System.out.println(E.x);
-					
+					px[j] = E.x;
+					py[j] = E.y;
+										
+					sx[j] = C.x;
+					sy[j] = C.y;
 				}
+
+				Point2D.Float maxProjected = getMaxValue(px, py, sx, sy)[0];
+				Point2D.Float maxShip = getMaxValue(px, py, sx, sy)[1];
 				
-				//TODO on a function
-				float max_projectionAB_value_x = projectionOnAB_points_x[0];
-				float max_projectionAB_value_y = projectionOnAB_points_y[0];
-				float min_projectionAB_value_x = projectionOnAB_points_x[0];
-				float min_projectionAB_value_y = projectionOnAB_points_y[0];
-				
-				float max_projectionAX_value_x = projectionOnAX_points_x[0];
-				float max_projectionAX_value_y = projectionOnAX_points_y[0];
-				float min_projectionAX_value_x = projectionOnAX_points_x[0];
-				float min_projectionAX_value_y = projectionOnAX_points_y[0];
-				
-				float max_ship_value_x = ship_points_x[0];
-				float max_ship_value_y = ship_points_y[0];
-				float min_ship_value_x = ship_points_x[0];
-				float min_ship_value_y = ship_points_y[0];
-				
-				float max_ship_value_x_AX = ship_points_x[0];
-				float max_ship_value_y_AX = ship_points_y[0];
-				float min_ship_value_x_AX = ship_points_x[0];
-				float min_ship_value_y_AX = ship_points_y[0];
-				
-				
-				for(int j = 0; j<projectionOnAB_points_x.length;j++) {
-					//Max Values on AB
-					if (projectionOnAB_points_x[j] > max_projectionAB_value_x) {
-						max_projectionAB_value_x = projectionOnAB_points_x[j];
-						max_ship_value_x = ship_points_x[j];
-					}
-					if (projectionOnAB_points_y[j] > max_projectionAB_value_y) {
-						max_projectionAB_value_y = projectionOnAB_points_y[j];
-						max_ship_value_y = ship_points_y[j];
-					}
-					//Min Values on AB
-					if (projectionOnAB_points_x[j] < min_projectionAB_value_x) {
-						min_projectionAB_value_x = projectionOnAB_points_x[j];
-						min_ship_value_x = ship_points_x[j];
-					}
-					if (projectionOnAB_points_y[j] < min_projectionAB_value_y) {
-						min_projectionAB_value_y = projectionOnAB_points_y[j];
-						min_ship_value_y = ship_points_y[j];
-					}
-				}
-				g2.drawLine((int)(max_ship_value_x),(int)(max_ship_value_y), (int)(max_projectionAB_value_x), (int)(max_projectionAB_value_y));
-				g2.drawLine((int)(min_ship_value_x),(int)(min_ship_value_y), (int)(min_projectionAB_value_x), (int)(min_projectionAB_value_y));
-				
-				for(int j = 0; j<projectionOnAB_points_x.length;j++) {
-					if(projectionOnAX_points_x[j] > max_projectionAX_value_x) {
-						max_projectionAX_value_x = projectionOnAX_points_x[j];
-						max_ship_value_x_AX = ship_points_x[j];
-					}
-					if(projectionOnAX_points_y[j] < max_projectionAX_value_y) {
-						max_projectionAX_value_y = projectionOnAX_points_y[j];
-						max_ship_value_y_AX = ship_points_y[j];
-					}		
-					if(projectionOnAX_points_x[j] < min_projectionAX_value_x) {
-						min_projectionAX_value_x = projectionOnAX_points_x[j];
-						min_ship_value_x_AX = ship_points_x[j];
-					}
-					if(projectionOnAX_points_y[j] > min_projectionAX_value_y) {
-						min_projectionAX_value_y = projectionOnAX_points_y[j];
-						min_ship_value_y_AX = ship_points_y[j];
-					}
-				}
-				
-				g2.drawLine((int)(max_ship_value_x_AX),(int)(max_ship_value_y_AX), (int)(max_projectionAX_value_x), (int)(max_projectionAX_value_y));
-				g2.drawLine((int)(min_ship_value_x_AX),(int)(min_ship_value_y_AX), (int)(min_projectionAX_value_x), (int)(min_projectionAX_value_y));
+				Point2D.Float minProjected = getMinValue(px, py, sx, sy)[0];
+				Point2D.Float minShip = getMinValue(px, py, sx, sy)[1];
+		
+				g2.drawLine((int)(maxShip.x),(int)(maxShip.y), (int)(maxProjected.x), (int)(maxProjected.y));
+				g2.drawLine((int)(minShip.x),(int)(minShip.y), (int)(minProjected.x), (int)(minProjected.y));
 				
 			}
 		}
+	}
+	
+	private Float[] getMaxValue(float[] points_x, float[] points_y, float[] shipPoints_x, float[] shipPoints_y) {
+
+		Point2D.Float MaxValue[] = new Point2D.Float[2];
+		Point2D.Float ValueP = new Point2D.Float(points_x[0],points_y[0]);
+		Point2D.Float ValueS = new Point2D.Float(shipPoints_x[0],shipPoints_y[0]);
+	
+		MaxValue[0] = ValueP; 
+		MaxValue[1] = ValueS;
+
+		for(int i = 0; i < points_x.length; i++) {
+			if (points_x[i] > MaxValue[0].x) {
+				MaxValue[0].x = points_x[i];
+				MaxValue[1].x = shipPoints_x[i];
+			}
+			if (points_y[i] < MaxValue[0].y) {
+				MaxValue[0].y = points_y[i];
+				MaxValue[1].y = shipPoints_y[i];
+			}
+			
+		}
+		return MaxValue;
+	}
+	
+	private Float[] getMinValue(float[] points_x, float[] points_y, float[] shipPoints_x, float[] shipPoints_y) {
+
+		Point2D.Float MinValue[] = new Point2D.Float[2];
+		Point2D.Float ValueP = new Point2D.Float(points_x[0],points_y[0]);
+		Point2D.Float ValueS = new Point2D.Float(shipPoints_x[0],shipPoints_y[0]);
+	
+		MinValue[0] = ValueP; 
+		MinValue[1] = ValueS;
+
+		for(int i = 0; i < points_x.length; i++) {
+			if (points_x[i] < MinValue[0].x) {
+				MinValue[0].x = points_x[i];
+				MinValue[1].x = shipPoints_x[i];
+			}
+			if (points_y[i] > MinValue[0].y) {
+				MinValue[0].y = points_y[i];
+				MinValue[1].y = shipPoints_y[i];
+			}
+			
+		}
+		return MinValue;
 	}
 	
 	private float angle(float x1, float y1, float x2 ,float y2) {
