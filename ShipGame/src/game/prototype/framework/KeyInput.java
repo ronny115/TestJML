019f02a777565a092b75d000ac506bd8546ded51
Ronny <Ronny@192.168.1.128>
2018-12-19 20:14:31 +0100

    Indenting

--- ShipGame/src/game/prototype/framework/KeyInput.java
+++ ShipGame/src/game/prototype/framework/KeyInput.java
@@ -8,207 +8,203 @@ import game.prototype.Handler;
 import game.prototype.objects.Projectile;
 
 public class KeyInput extends KeyAdapter {
-	
-	private PlayerObject tempPlayer;
-	private Handler handler;
-	private float topSpeed = 0, deltaSpeed = 0, vely = 0, velx =0;
-	//Movement
-	private boolean upKeyPressed, downKeyPressed, leftKeyPressed, rightKeyPressed;
-	private boolean upKeyReleased, downKeyReleased, leftKeyReleased, rightKeyReleased;
-	//Shooting
-	private boolean spaceKeyPressed;
-	private Point2D.Float[] firePos = new Point2D.Float[5];
-	
-	public KeyInput(Handler handler) {
-		this.handler = handler;
-	}
-	
-	public void updateInput() {
-	//Movement
-		//Pressed
-		if (upKeyPressed == true) {
-			move("up");
-			if (upKeyPressed == true && leftKeyPressed == true) {
-				move("up");
-				move("left");
-			} else if (upKeyPressed == true && rightKeyPressed == true) {
-				move("up");
-				move("right");
-			} else if (leftKeyReleased == true) {
-				move("decelerateLeft");
-			} else if (rightKeyReleased == true) {
-				move("decelerateRight");
-			}
-		}			
-		if (downKeyPressed == true) {
-			move("down");
-		}
-		if (leftKeyPressed == true) {
-			move("left");
-		}
-		if (rightKeyPressed == true) {
-			move("right");
-		}
-		//Released
-		if (upKeyReleased == true) {
-			move("decelerateUp");		
-		}
-		if (downKeyReleased == true) {
-			move("decelerateDown");		
-		}
-		if (leftKeyReleased == true && leftKeyPressed == false &&
-			rightKeyPressed == false) 
-		{	
-			move("decelerateLeft");
-		}
-		if (rightKeyReleased == true && rightKeyPressed == false && 
-			leftKeyPressed == false) 
-		{	
-			move("decelerateRight");
-		}
-	//Shooting
-		if (spaceKeyPressed == true) {
-			handler.addObject(new Projectile(firePos[1].x, firePos[1].y, 5, 20, 20,
-											"player", Helper.bulletAngle(handler.player.get(0).points()), 
-											ObjectId.Projectile));
-			
-			spaceKeyPressed = false;
-		}	
-	}
-	
-	public void keyPressed(KeyEvent e) {
-		for (int i = 0; i < handler.player.size(); i++) {
-			tempPlayer = handler.player.get(i);
-			if (tempPlayer.getId() == PlayerId.PlayerShip) {	
-				if (e.getKeyCode() == KeyEvent.VK_UP) {
-					upKeyPressed = true;
-					upKeyReleased = false;
-				}
-				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-					downKeyPressed = true;
-					downKeyReleased = false;
-				}
-				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-					leftKeyPressed = true;	
-					leftKeyReleased = false;	
-				}
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-					rightKeyPressed = true;
-					rightKeyReleased = false;
-				}
-				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-					System.exit(1);
-				}
-				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-					firePos = tempPlayer.points();
-					spaceKeyPressed = true;	
-				}
-			}
-		}			
-	}
-	
-	public void keyReleased(KeyEvent e) {	
-		for (int i = 0; i < handler.player.size(); i++) {
-			tempPlayer = handler.player.get(i);
-			if (tempPlayer.getId() == PlayerId.PlayerShip) {	
-				if (e.getKeyCode() == KeyEvent.VK_UP) {
-					upKeyPressed = false;
-					upKeyReleased = true;
-				}
-				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-					downKeyPressed = false;
-					downKeyReleased = true;
-				}
-				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-					leftKeyPressed = false;
-					leftKeyReleased = true;	
-				}
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-					rightKeyPressed = false;
-					rightKeyReleased = true;		
-				}
-				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-					spaceKeyPressed = false;			
-				}
-			}
-		}
-	}
-	
-	public void setSpeed(float topSpeed, float deltaSpeed) {
-		this.topSpeed = topSpeed;
-		this.deltaSpeed = deltaSpeed;
-	}
-	
-	private void move(String move) {
-		switch(move) {
-		case "up":
-			vely += deltaSpeed;
-			tempPlayer.setVelY(vely);
-			if (vely >= topSpeed) {
-				vely = (float) topSpeed;
-				tempPlayer.setVelY(vely);
-			}
-			break;
-		case "down":
-			vely += -deltaSpeed*2;
-			tempPlayer.setVelY(vely);
-			if (vely <= -topSpeed/10) {
-				vely = (float) -topSpeed/10;
-				tempPlayer.setVelY(vely);
-			}
-			break;
-		case "left":
-			velx -= deltaSpeed/50;
-			tempPlayer.setVelX(velx);
-			if (velx <= -topSpeed/100) {
-				velx = (float) (-topSpeed/100);
-				tempPlayer.setVelX(velx);
-			}	
-			break;
-		case "right":
-			velx += deltaSpeed/50;
-			tempPlayer.setVelX(velx);
-			if (velx >= topSpeed/100) {
-				velx = (float) (topSpeed/100);
-				tempPlayer.setVelX(velx);
-			}
-			break;
-		case "decelerateUp":	
-			vely -= deltaSpeed/5;
-			tempPlayer.setVelY(vely);
-			if (vely < 0) {
-				vely = 0;
-				tempPlayer.setVelY(vely);
-				upKeyReleased = false; 
-			}
-			break;
-		case "decelerateDown":
-			vely += deltaSpeed/5;
-			tempPlayer.setVelY(vely);
-			if (vely > 0) {
-				vely = 0;
-				tempPlayer.setVelY(vely);
-				downKeyReleased = false;
-			}
-			break;
-		case "decelerateLeft":
-			velx += deltaSpeed/50;
-			tempPlayer.setVelX(velx);
-			if (velx > 0) {
-				velx = 0;
-				tempPlayer.setVelX(velx);
-				leftKeyReleased = false;
-			}
-			break;
-		case "decelerateRight":
-			velx -= deltaSpeed/50;
-			tempPlayer.setVelX(velx);
-			if (velx < 0) {
-				velx = 0;
-				tempPlayer.setVelX(velx);
-				rightKeyReleased = false;
-			}
-			break;
-		}
-	}
+
+    private PlayerObject tempPlayer;
+    private Handler handler;
+    private float topSpeed = 0, deltaSpeed = 0, vely = 0, velx = 0;
+    // Movement
+    private boolean upKeyPressed, downKeyPressed, leftKeyPressed, rightKeyPressed;
+    private boolean upKeyReleased, downKeyReleased, leftKeyReleased, rightKeyReleased;
+    // Shooting
+    private boolean spaceKeyPressed;
+    private Point2D.Float[] firePos = new Point2D.Float[5];
+
+    public KeyInput(Handler handler) {
+        this.handler = handler;
+    }
+
+    public void updateInput() {
+        // Movement
+        // Pressed
+        if (upKeyPressed == true) {
+            move("up");
+            if (upKeyPressed == true && leftKeyPressed == true) {
+                move("up");
+                move("left");
+            } else if (upKeyPressed == true && rightKeyPressed == true) {
+                move("up");
+                move("right");
+            } else if (leftKeyReleased == true) {
+                move("decelerateLeft");
+            } else if (rightKeyReleased == true) {
+                move("decelerateRight");
+            }
+        }
+        if (downKeyPressed == true) {
+            move("down");
+        }
+        if (leftKeyPressed == true) {
+            move("left");
+        }
+        if (rightKeyPressed == true) {
+            move("right");
+        }
+        // Released
+        if (upKeyReleased == true) {
+            move("decelerateUp");
+        }
+        if (downKeyReleased == true) {
+            move("decelerateDown");
+        }
+        if (leftKeyReleased == true && leftKeyPressed == false && rightKeyPressed == false) {
+            move("decelerateLeft");
+        }
+        if (rightKeyReleased == true && rightKeyPressed == false && leftKeyPressed == false) {
+            move("decelerateRight");
+        }
+        // Shooting
+        if (spaceKeyPressed == true) {
+            handler.addObject(new Projectile(firePos[1].x, firePos[1].y, 5, 20, 20, "player",
+                                            Helper.bulletAngle(handler.player.get(0).points()), 
+                                            ObjectId.Projectile));
+
+            spaceKeyPressed = false;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        for (int i = 0; i < handler.player.size(); i++) {
+            tempPlayer = handler.player.get(i);
+            if (tempPlayer.getId() == PlayerId.PlayerShip) {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    upKeyPressed = true;
+                    upKeyReleased = false;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    downKeyPressed = true;
+                    downKeyReleased = false;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    leftKeyPressed = true;
+                    leftKeyReleased = false;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    rightKeyPressed = true;
+                    rightKeyReleased = false;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    System.exit(1);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    firePos = tempPlayer.points();
+                    spaceKeyPressed = true;
+                }
+            }
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        for (int i = 0; i < handler.player.size(); i++) {
+            tempPlayer = handler.player.get(i);
+            if (tempPlayer.getId() == PlayerId.PlayerShip) {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    upKeyPressed = false;
+                    upKeyReleased = true;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    downKeyPressed = false;
+                    downKeyReleased = true;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    leftKeyPressed = false;
+                    leftKeyReleased = true;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    rightKeyPressed = false;
+                    rightKeyReleased = true;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    spaceKeyPressed = false;
+                }
+            }
+        }
+    }
+
+    public void setSpeed(float topSpeed, float deltaSpeed) {
+        this.topSpeed = topSpeed;
+        this.deltaSpeed = deltaSpeed;
+    }
+
+    private void move(String move) {
+        switch (move) {
+            case "up":
+                vely += deltaSpeed;
+                tempPlayer.setVelY(vely);
+                if (vely >= topSpeed) {
+                    vely = (float) topSpeed;
+                    tempPlayer.setVelY(vely);
+                }
+                break;
+            case "down":
+                vely += -deltaSpeed * 2;
+                tempPlayer.setVelY(vely);
+                if (vely <= -topSpeed / 10) {
+                    vely = (float) -topSpeed / 10;
+                    tempPlayer.setVelY(vely);
+                }
+                break;
+            case "left":
+                velx -= deltaSpeed / 50;
+                tempPlayer.setVelX(velx);
+                if (velx <= -topSpeed / 100) {
+                    velx = (float) (-topSpeed / 100);
+                    tempPlayer.setVelX(velx);
+                }
+                break;
+            case "right":
+                velx += deltaSpeed / 50;
+                tempPlayer.setVelX(velx);
+                if (velx >= topSpeed / 100) {
+                    velx = (float) (topSpeed / 100);
+                    tempPlayer.setVelX(velx);
+                }
+                break;
+            case "decelerateUp":
+                vely -= deltaSpeed / 5;
+                tempPlayer.setVelY(vely);
+                if (vely < 0) {
+                    vely = 0;
+                    tempPlayer.setVelY(vely);
+                    upKeyReleased = false;
+                }
+                break;
+            case "decelerateDown":
+                vely += deltaSpeed / 5;
+                tempPlayer.setVelY(vely);
+                if (vely > 0) {
+                    vely = 0;
+                    tempPlayer.setVelY(vely);
+                    downKeyReleased = false;
+                }
+                break;
+            case "decelerateLeft":
+                velx += deltaSpeed / 50;
+                tempPlayer.setVelX(velx);
+                if (velx > 0) {
+                    velx = 0;
+                    tempPlayer.setVelX(velx);
+                    leftKeyReleased = false;
+                }
+                break;
+            case "decelerateRight":
+                velx -= deltaSpeed / 50;
+                tempPlayer.setVelX(velx);
+                if (velx < 0) {
+                    velx = 0;
+                    tempPlayer.setVelX(velx);
+                    rightKeyReleased = false;
+                }
+                break;
+        }
+    }
 }
--- ShipGame/src/game/prototype/framework/TextureManager.java
+++ ShipGame/src/game/prototype/framework/TextureManager.java
@@ -5,20 +5,19 @@ import java.awt.image.BufferedImage;
 import game.prototype.ResourceLoader;
 
 public class TextureManager {
-	
-	private SpriteSheet pS, eS, explS;
-	
-	private BufferedImage player_sheet = null;
-	private BufferedImage enemy_sheet = null;
-	private BufferedImage explosion_sheet = null;
-	
-	public BufferedImage[] player = new BufferedImage[1];
-	public BufferedImage[] explosiveMine = new BufferedImage[1];
-	public BufferedImage[] ghostIdle = new BufferedImage[15];
-	public BufferedImage[] bullet = new BufferedImage[3];
-	public BufferedImage[] explosion = new BufferedImage[8];
-	
-	public TextureManager() {
+    private SpriteSheet pS, eS, explS;
+    
+    private BufferedImage player_sheet = null;
+    private BufferedImage enemy_sheet = null;
+    private BufferedImage explosion_sheet = null;
+    
+    public BufferedImage[] player = new BufferedImage[1];
+    public BufferedImage[] explosiveMine = new BufferedImage[1];
+    public BufferedImage[] ghostIdle = new BufferedImage[15];
+    public BufferedImage[] bullet = new BufferedImage[3];
+    public BufferedImage[] explosion = new BufferedImage[8];
+    
+    public TextureManager() {
 		ResourceLoader loader = new ResourceLoader();
 		try {
 			player_sheet = loader.loadImage("/player_sheet.png");
@@ -32,30 +31,27 @@ public class TextureManager {
 		eS = new SpriteSheet(enemy_sheet);
 		explS = new SpriteSheet(explosion_sheet);
 		getTextures();
-	}
-
-	private void getTextures() {
+		}
+    
+    private void getTextures() {
 		int count = 0;
 		player[0] = pS.grabImage(1, 1, 30, 40);//player ship
 		bullet[0] = pS.grabImage(1, 4, 30, 40);//GreenBullet
 		bullet[1] = pS.grabImage(2, 4, 30, 40);//RedBullet
 		//Enemy
 		for (int i = 0; i < 10; i++) {
-			ghostIdle[i] = eS.grabImage(i+1, 2, 50, 50);
-		}
+		    ghostIdle[i] = eS.grabImage(i+1, 2, 50, 50);
+		    }
 		for (int i = 0; i < 5; i++) {
-			ghostIdle[10+i] = eS.grabImage(i+1, 3, 50, 50);
-		}
-		
+		    ghostIdle[10+i] = eS.grabImage(i+1, 3, 50, 50);
+		    }
 		explosiveMine[0] = eS.grabImage(1, 1, 50, 50); //mine
-		
 		for (int i = 0; i < 2; i++) { //explosion last frame invisible
-			for (int j = 0; j < 4; j++) {		
-				explosion[count] = explS.grabImage(j+1, i+1, 60, 60);
-				count++;
-			}
-		}
+		    for (int j = 0; j < 4; j++) {
+		        explosion[count] = explS.grabImage(j+1, i+1, 60, 60);
+		        count++;
+		        }
+		    }
 		count = 0;
-	}
-	
-}
+		}
+    }
